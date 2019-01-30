bfb885a5b3695211d99efb9a931d14e83a3199a2
ben Petersen <ben83pete@gmail.com>
2019-01-30 08:51:59 -0500

    C# day 1

--- HelloWorldProject/Program.cs
+++ HelloWorldProject/Program.cs
@@ -8,8 +8,33 @@ namespace HelloWorldProject
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static int A = 7;
+
+        static void WhileExample()
+        { var counter = 0;
+            while (counter < 10) { Console.WriteLine($"Count is {counter}");
+                //counter = counter + 1;
+            }
         }
+
+        static void Print()
+        {
+            Console.WriteLine($"The value of A is {A}");
+        }
+
+        static void Main(string[] args) /* Method called Main, 
+            this is the start of the program. */
+        {
+            WhileExample();
+            var counter = 0;            
+            var B = 13;
+            var C = A * B;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("by Ben");
+            Console.WriteLine($"For A = {A} and B = {B}, A * B={C}");
+            Console.WriteLine($"Value of counter is {counter}");
+
+         }
+    
     }
 }
